87554d5986ffedb23e0e42246ad3c485b6f54803
unknown <nahidujjamanraj@gmail.com>
2024-11-18 19:50:15 +0600

    In this program i make a function for rectangular calculation

--- javasCRIPT/P1.js
+++ javasCRIPT/P1.js
@@ -11,3 +11,16 @@ function calculate(){
     let areaResult=document.getElementById('triangle_area')
     areaResult.innerText=area
 }
+// rectangle function 
+
+function calculate2(){
+    let input2_1=document.getElementById('input-2.1').value;
+    let input2_1_text=parseFloat(input2_1)
+
+    let input2_2=document.getElementById('input2.2').value;
+    let input2_2Text=parseFloat(input2_2)
+
+    let Area=input2_1 * input2_2Text;
+    let areaResult=document.getElementById('rectangle_area')
+    areaResult.innerText=Area
+}
