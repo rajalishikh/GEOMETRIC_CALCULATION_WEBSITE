db39fadc78ea43311fead09c2680d1e0a629bd72
unknown <nahidujjamanraj@gmail.com>
2024-11-18 18:27:34 +0600

    In this program I make a function that calculate Triangle area

--- javasCRIPT/P1.js
+++ javasCRIPT/P1.js
@@ -1,3 +1,13 @@
 function calculate(){
-    console.log('calculate ')
+    let input1=document.getElementById('input-1').value
+    let input1textBhumi=parseFloat(input1)
+    
+    let input2=document.getElementById('input2').value;
+    let input2textHeight=parseFloat(input2)
+
+    let area=0.5*input1textBhumi*input2textHeight;
+    console.log(area)
+
+    let areaResult=document.getElementById('triangle_area')
+    areaResult.innerText=area
 }
